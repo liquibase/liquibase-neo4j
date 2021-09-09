542da4b4d6cacc908c074c8c906293e019e4761e
Florent Biville <florent.biville@neo4j.com>
2021-09-09 11:16:46 +0200

    Upgrade JDBC driver to include RETURN clause detection fix
    This also includes the support of the 4.x URI schemes and fixes
    the JDBC driver instantiation when routing is needed.
    
    Fixes #14

--- src/test/groovy/liquibase/ext/neo4j/database/Neo4jDatabaseTest.groovy
+++ src/test/groovy/liquibase/ext/neo4j/database/Neo4jDatabaseTest.groovy
@@ -55,10 +55,10 @@ class Neo4jDatabaseTest extends Specification {
         "jdbc:neo4j:bolt://localhost/"              | "org.neo4j.jdbc.bolt.BoltDriver"
         "jdbc:neo4j:bolt+s://localhost/"            | "org.neo4j.jdbc.bolt.BoltDriver"
         "jdbc:neo4j:bolt+ssc://localhost/"          | "org.neo4j.jdbc.bolt.BoltDriver"
-        "jdbc:neo4j:bolt+routing://localhost:1234/" | "org.neo4j.jdbc.bolt.BoltDriver"
-        "jdbc:neo4j:neo4j://localhost/"             | "org.neo4j.jdbc.bolt.BoltDriver"
-        "jdbc:neo4j:neo4j+s://localhost/"           | "org.neo4j.jdbc.bolt.BoltDriver"
-        "jdbc:neo4j:neo4j+ssc://localhost/"         | "org.neo4j.jdbc.bolt.BoltDriver"
+        "jdbc:neo4j:bolt+routing://localhost:1234/" | "org.neo4j.jdbc.boltrouting.BoltRoutingNeo4jDriver"
+        "jdbc:neo4j:neo4j://localhost/"             | "org.neo4j.jdbc.boltrouting.BoltRoutingNeo4jDriver"
+        "jdbc:neo4j:neo4j+s://localhost/"           | "org.neo4j.jdbc.boltrouting.BoltRoutingNeo4jDriver"
+        "jdbc:neo4j:neo4j+ssc://localhost/"         | "org.neo4j.jdbc.boltrouting.BoltRoutingNeo4jDriver"
         "jdbc:mysql://localhost/db"                 | null
         null                                        | null
     }
