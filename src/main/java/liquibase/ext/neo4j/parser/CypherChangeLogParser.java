b00791d9588d440ff45cc29426759aca41b84bbf
clarence.charles <clarence.charles@rakuten.com>
2022-02-22 20:12:41 +0100

    Make <include> and <includeAll> work with .cypher files
    Fixes #117

---
+++ src/main/java/liquibase/ext/neo4j/parser/CypherChangeLogParser.java
@@ -0,0 +1,12 @@
+package liquibase.ext.neo4j.parser;
+
+import liquibase.parser.core.sql.SqlChangeLogParser;
+import liquibase.resource.ResourceAccessor;
+
+public class CypherChangeLogParser extends SqlChangeLogParser {
+
+    @Override
+    public boolean supports(String changeLogFile, ResourceAccessor resourceAccessor) {
+        return changeLogFile.endsWith(".cypher");
+    }
+}
