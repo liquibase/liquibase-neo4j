b03d30f4c8dbebd50be2a0da8b623febe67209e9
Florent Biville <florent.biville@neo4j.com>
2023-03-31 17:31:54 +0200

    Fix buggy time comparisons in tests

--- src/test/groovy/liquibase/ext/neo4j/database/jdbc/Offsets.groovy
+++
@@ -1,8 +0,0 @@
-package liquibase.ext.neo4j.database.jdbc
-
-class Offsets {
-
-    static int currentUtcOffsetMillis() {
-        return TimeZone.getDefault().getOffset(System.currentTimeMillis())
-    }
-}
