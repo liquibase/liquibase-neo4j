c3dab97738d11aa3700d882d9284326b6ee50af1
Florent Biville <florent.biville@neo4j.com>
2023-05-10 09:54:33 +0200

    Fix JSON/YAML parsing of Cypher check attribute
    Fixes #388

--- src/main/java/liquibase/ext/neo4j/precondition/CypherCheckPrecondition.java
+++ src/main/java/liquibase/ext/neo4j/precondition/CypherCheckPrecondition.java
@@ -2,6 +2,9 @@ package liquibase.ext.neo4j.precondition;
 
 import liquibase.precondition.core.SqlPrecondition;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class CypherCheckPrecondition extends SqlPrecondition {
 
     @Override
@@ -15,5 +18,20 @@ public class CypherCheckPrecondition extends SqlPrecondition {
 
     public String getCypher() {
         return getSql();
+    }
+
+    @Override
+    public Set<String> getSerializableFields() {
+        Set<String> fields = new HashSet<>(super.getSerializableFields());
+        fields.add("cypher");
+        return fields;
+    }
+
+    @Override
+    protected Class<?> getSerializableFieldDataTypeClass(String field) {
+        if (field.equals("cypher")) {
+            return String.class;
+        }
+        return super.getSerializableFieldDataTypeClass(field);
     }
 }
