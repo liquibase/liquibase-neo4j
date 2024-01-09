3613819dbee2fbb8f8fa944fffadb87df8c3ddec
Florent Biville <florent.biville@neo4j.com>
2024-01-09 15:05:11 +0100

    Add tests for batched property renames targeting nodes & rels

--- src/test/groovy/liquibase/ext/neo4j/e2e/RenamePropertyIT.groovy
+++ src/test/groovy/liquibase/ext/neo4j/e2e/RenamePropertyIT.groovy
@@ -125,7 +125,6 @@ class RenamePropertyIT extends Neo4jContainerSpec {
         format << ["json", "xml", "yaml"]
     }
 
-
     def "runs batched migrations renaming properties of all entities"() {
         given:
         def command = new CommandScope(UpdateCommandStep.COMMAND_NAME)
@@ -156,6 +155,84 @@ class RenamePropertyIT extends Neo4jContainerSpec {
                         rel_props   : [date: 'now'],
                         start_labels: ['Movie'],
                         start_props : [date: 'today'],
+                        end_labels  : ['Person'],
+                        end_props   : [:]
+                ]
+        ]
+
+        where:
+        format << ["json", "xml", "yaml"]
+    }
+
+    def "runs batched migrations renaming properties of nodes only"() {
+        given:
+        def command = new CommandScope(UpdateCommandStep.COMMAND_NAME)
+                .addArgumentValue(DbUrlConnectionCommandStep.URL_ARG, "jdbc:neo4j:${neo4jContainer.getBoltUrl()}".toString())
+                .addArgumentValue(DbUrlConnectionCommandStep.USERNAME_ARG, "neo4j")
+                .addArgumentValue(DbUrlConnectionCommandStep.PASSWORD_ARG, PASSWORD)
+                .addArgumentValue(DatabaseChangelogCommandStep.CHANGELOG_FILE_ARG, "/e2e/rename-property/changeLog-node-batched.${format}".toString())
+                .setOutput(System.out)
+        command.execute()
+
+        expect:
+        def rows = queryRunner.getRows("""
+            MATCH (s)-[r]->(e)
+            WHERE none(label IN labels(s) WHERE label STARTS WITH "__Liquibase")
+            RETURN {
+                rel_type: type(r),
+                rel_props: properties(r), 
+                start_labels: labels(s),
+                start_props: properties(s), 
+                end_labels: labels(e),
+                end_props: properties(e)
+            } AS result
+        """)
+
+        rows["result"] == [
+                [
+                        rel_type    : "SEEN_BY",
+                        rel_props   : [calendar_date: 'now'],
+                        start_labels: ['Movie'],
+                        start_props : [date: 'today'],
+                        end_labels  : ['Person'],
+                        end_props   : [:]
+                ]
+        ]
+
+        where:
+        format << ["json", "xml", "yaml"]
+    }
+
+    def "runs batched migrations renaming properties of rels only"() {
+        given:
+        def command = new CommandScope(UpdateCommandStep.COMMAND_NAME)
+                .addArgumentValue(DbUrlConnectionCommandStep.URL_ARG, "jdbc:neo4j:${neo4jContainer.getBoltUrl()}".toString())
+                .addArgumentValue(DbUrlConnectionCommandStep.USERNAME_ARG, "neo4j")
+                .addArgumentValue(DbUrlConnectionCommandStep.PASSWORD_ARG, PASSWORD)
+                .addArgumentValue(DatabaseChangelogCommandStep.CHANGELOG_FILE_ARG, "/e2e/rename-property/changeLog-rel-batched.${format}".toString())
+                .setOutput(System.out)
+        command.execute()
+
+        expect:
+        def rows = queryRunner.getRows("""
+            MATCH (s)-[r]->(e)
+            WHERE none(label IN labels(s) WHERE label STARTS WITH "__Liquibase")
+            RETURN {
+                rel_type: type(r),
+                rel_props: properties(r), 
+                start_labels: labels(s),
+                start_props: properties(s), 
+                end_labels: labels(e),
+                end_props: properties(e)
+            } AS result
+        """)
+
+        rows["result"] == [
+                [
+                        rel_type    : "SEEN_BY",
+                        rel_props   : [date: 'now'],
+                        start_labels: ['Movie'],
+                        start_props : [calendar_date: 'today'],
                         end_labels  : ['Person'],
                         end_props   : [:]
                 ]
