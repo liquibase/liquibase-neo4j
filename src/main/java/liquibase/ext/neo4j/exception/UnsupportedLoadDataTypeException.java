db8222e50612cf5efefb87e50179816815e36bf5
Florent Biville <florent.biville@neo4j.com>
2025-01-17 16:01:30 +0100

    fix: remove fixable deprecation warnings
    `RawSqlStatement` is not replaceable yet, as its parameterized
    replacement does not validate without parameters.
    
    The `PreconditionFailedException` constructor this extension uses
    is going to be un-deprecated in the next release.

--- src/main/java/liquibase/ext/neo4j/exception/UnsupportedLoadDataTypeException.java
+++ src/main/java/liquibase/ext/neo4j/exception/UnsupportedLoadDataTypeException.java
@@ -2,6 +2,7 @@ package liquibase.ext.neo4j.exception;
 
 import liquibase.exception.UnexpectedLiquibaseException;
 
+@SuppressWarnings("serial")
 public class UnsupportedLoadDataTypeException extends UnexpectedLiquibaseException {
     public UnsupportedLoadDataTypeException(String message, Object... args) {
         super(String.format(message, args));
